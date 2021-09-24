7211b791d6736466de31f124b2c007ac0a33764c
Rojas-Andres <andresfelipe200004@gmail.com>
2021-09-23 21:17:03 -0500

    Form para añadir vendedor

--- src/pages/anadirVendedor.jsx
+++ src/pages/anadirVendedor.jsx
@@ -1,5 +1,27 @@
 const AnadirVendedor =()=>{
-    return (<h3>Añadir vendedorasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</h3>);
+    return(
+        <div>
+            <div>
+                <h2>Añadir Vendedor</h2>
+            </div>
+            <div>
+                <span>Identificacion:10583868</span>
+                <form>
+                    <label for="fname">Nombre</label>
+                    <input type="text" id="fname" name="fname"/>
+                    <br />
+                    <label for="lname">Especialidad</label>
+                    <input type="text" id="lname" name="lname"/>
+                    <br />
+                    <label for="lname">Telefono</label>
+                    <input type="text" id="lname" name="lname"/>
+                    <br />
+                    <label for="lname">Fecha Ingreso</label>
+                    <input type="date" id="lname" name="lname"/>
+                </form>
+            </div>
+        </div>
+    );
   }
   
   export default AnadirVendedor;
