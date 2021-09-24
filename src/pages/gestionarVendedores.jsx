7def3e74ead56d93958a84b2148e67bf642a8679
Rojas-Andres <andresfelipe200004@gmail.com>
2021-09-23 20:37:09 -0500

    Añadiendo css a gestionar vendedores

--- src/pages/gestionarVendedores.jsx
+++ src/pages/gestionarVendedores.jsx
@@ -1,29 +1,48 @@
 //La gestion de ventas será nuestro pagina de inicio o index
+import chulito from 'media/chulito.png';
 
 const GestionarVendedor =()=>{
     return(
         <section>
-            <h1>GESTIONAR VENDEDORES</h1>
-            <table>
-                <tr>
-                    <th>Identificacion</th>
-                    <th>Nombre</th>
-                    <th>Especialidad</th>
-                    <th>Telefono</th>
-                    <th>Fecha de Ingreso</th>
-                    <th>Actualizar</th>
-                </tr>
-                <tr>
-
-                    <td>10527868</td>
-                    <td>Ruby Medina</td>
-                    <td>Venta de calzado</td>
-                    <td>312704055</td>
-                    <td>22/09/2021</td>
-                    <td><button>Actualizar</button></td>
-                </tr>
-
-            </table>
+            <div className="encabezado_vendedores">
+                <h1>GESTIONAR VENDEDORES</h1>
+            </div>
+            <div className="color_tabla">
+                <table>
+                    <tr className="encabezado_tabla">
+                        <th>Identificacion</th>
+                        <th>Nombre</th>
+                        <th>Especialidad</th>
+                        <th>Telefono</th>
+                        <th>Fecha de Ingreso</th>
+                        <th>Actualizar</th>
+                    </tr>
+                    <tr>
+                        <td>10527868</td>
+                        <td>Ruby Medina</td>
+                        <td>Venta de calzado</td>
+                        <td>312704055</td>
+                        <td>22/09/2021</td>
+                        <td><div className="contenedorImagen"><img src={chulito} alt="" /></div></td>
+                    </tr>
+                    <tr>
+                        <td>10527868</td>
+                        <td>Ruby Medina</td>
+                        <td>Venta de calzado</td>
+                        <td>312704055</td>
+                        <td>22/09/2021</td>
+                        <td><div className="contenedorImagen"><img src={chulito} alt="" /></div></td>
+                    </tr>
+                    <tr>
+                        <td>10527868</td>
+                        <td>Ruby Medina</td>
+                        <td>Venta de calzado</td>
+                        <td>312704055</td>
+                        <td>22/09/2021</td>
+                        <td className='td_actualizar'><div className="contenedorImagen"><img src={chulito} alt="" /></div></td>
+                    </tr>
+                </table>
+            </div>
         </section>
 
     );
