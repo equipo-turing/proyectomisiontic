877cf52d8ebd26279b78e426d0edb0a068495a0b
walterYangana <medinawalter3@gmail.com>
2021-10-03 23:12:20 -0500

    final

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -29,7 +29,7 @@ const Index = () => {
           </div >
           
 
-          <input type="submit" value="Registrase"/>
+          <input type="submit" value="Registrarse"/>
          
         </form>
       </div>
