7a4826e15cfb844116fbfd6b8db18867eb3d916b
walterYangana <medinawalter3@gmail.com>
2021-10-10 00:28:06 -0500

    toast con mensaje de guardado de producto ,guarda desde una lista de productos,objetos

--- src/pages/gestionar_producto.jsx
+++ src/pages/gestionar_producto.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect,useState,useRef} from 'react';
 import 'styles/estiloIndex.css';
 import plus_circle from 'media/plus-circle1.png';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 //import TablaProductos from 'components/TablaProductos'
 
@@ -21,21 +23,28 @@ const bdproducto=()=>[
         estado:"no disponible",
        
     },
+    {
+        identificacion:"12345",
+        descripcion:"chanclas",
+        valorUnitario:"45000",
+        estado:"no disponible",
+       
+    }
    
     
     
 ];
 
 //Tabla para mostrar los productos
-const TablaProductos = () => {
+const TablaProductos = ({setMostrarTabla,mostrarTabla,listaProducto}) => {
     return (
         <div>
                 <div>
                   <div className="contenedorImagenTitulo">                
                     <div className="iconoVentas">
-                      <button>
+                      <button onClick={()=>{setMostrarTabla(!mostrarTabla)}}>
                           <img src={plus_circle} alt="" />
-                          </button>
+                     </button>
                     </div>
                   
                     <div className="tituloGestionarProducto">
@@ -53,7 +62,21 @@ const TablaProductos = () => {
                     <th>Estado</th>                    
                   </tr>
                 </thead>
-                <tbody>                  
+                <tbody>
+                    {listaProducto.map((bdproducto)=>{
+                        return(
+                            <tr>
+                                <td>{bdproducto.identificacion}</td>
+                                <td>{bdproducto.descripcion}</td>
+                                <td>{bdproducto.valorUnitario}</td>
+                                <td>{bdproducto.estado}</td>
+                            </tr> 
+                            
+
+                        )
+
+                    })}
+                    <tr></tr>                  
                       
                  
               </tbody>
@@ -62,63 +85,83 @@ const TablaProductos = () => {
         </div>
     )
 }
+//formulario donde se crean los productos
+const FormularioCrearProducto=({setMostrarTabla,mostrarTabla,setProducto,listaProducto})=>{
+    const [identificacion,setIdentificacion]=useState('')
+    const [descripcion,setDescripcion]=useState('')
+    const [valorUnitario,setValorUnitario]=useState('')
+    const [estado,setEstado]=useState('')
+
+    const guardarProducto=()=>{    
+        setProducto([...listaProducto,{identificacion:identificacion,descripcion:descripcion,valorUnitario:valorUnitario,estado:estado}])    
+        toast.success("Producto Guardado !!");
+
+    }
+
+   
+    return(
+    
+        <div  className="formularioCrearVentas">
+            
+            <div className="contenedorTituloRegistroVenta">
+            <h1>Crear Producto</h1>                    
+            </div>
+
+            <form >
+            <label className="labelCampos" htmlFor="codigo">
+                Identificacion
+                <input value={identificacion} onChange={(e)=>{setIdentificacion(e.target.value)}} name='identificacion' className="camposRegistroVenta" type="text" />
+            </label> 
+
+            <label className="labelCampos" htmlFor="descripcion">
+                Descripción
+                <input value={descripcion} onChange={(e)=>{setDescripcion(e.target.value)}} name='descripcion' className="camposRegistroVenta" type="text" />
+            </label>
+
+            <label className="labelCampos" htmlFor="valor unitario">
+                Valor Unitario
+                <input value={valorUnitario} onChange={(e)=>{setValorUnitario(e.target.value)}} className="camposRegistroVenta" type="text" name='valorUnitario' />
+            </label>
+
+            <label className="labelCampos"  htmlFor="estado">
+                Estado
+                <input value={estado} onChange={(e)=>{setEstado(e.target.value)}} name='estado' className="camposRegistroVenta" type="text" />
+            </label>                
+            
+
+            <div className="contBotonGuardarVenta">
+                
+                <button  className="botonCancelar" type="button" value="Cancelar" >Cancelar</button>
+                <button onClick={()=>{guardarProducto();setMostrarTabla(!mostrarTabla)}} className="botonGuardar" type="button" value="Guardar">Crear</button>
+
+            </div>          
+
+            </form>
+
+       </div>
+)
+
+}
 
 
 const Gestionar_producto = () => {
-
-    const [identificacion,setIdentificacion]=useState('')
-    const [valorUnitario,setValorUnitario]=useState()
-   
+    const [mostrarTabla,setMostrarTabla]=useState(true)
+    const [producto,setProducto]=useState([])//es un arreglo que almacenará los productos que vengan de la bd
 
     useEffect(()=>{
-        console.log("identificacion ",identificacion)        
-       
-       },[identificacion]); 
+        setProducto(bdproducto);
+    },[])
 
-    const enviarDatos=()=>{
-        console.log("codigo: ",identificacion)
-    }
-
+   
     return (
-       
-        <div  className="formularioCrearVentas">
-        
-                <div className="contenedorTituloRegistroVenta">
-                <h1>Crear Producto</h1>                    
-                </div>
+        <div>         
 
-                <form >
-                <label className="labelCampos" htmlFor="codigo">
-                    Identificacion
-                    <input onChange={(e)=>{setIdentificacion(e.target.value)}} name='identificacion' className="camposRegistroVenta" type="text" />
-                </label> 
+            {mostrarTabla ?  <TablaProductos setMostrarTabla={setMostrarTabla} mostrarTabla={mostrarTabla} listaProducto={producto} /> 
+            :<FormularioCrearProducto setMostrarTabla={setMostrarTabla} mostrarTabla={mostrarTabla} setProducto={setProducto } listaProducto={producto}/>}
+            <ToastContainer  position="top-center"  autoClose={3000} />            
 
-                <label className="labelCampos" htmlFor="descripcion">
-                    Descripción
-                    <input name='descripcion' className="camposRegistroVenta" type="text" />
-                </label>
-        
-                <label className="labelCampos" htmlFor="valor unitario">
-                    Valor Unitario
-                    <input onChange={(e)=>{setValorUnitario(e.target.value)}} className="camposRegistroVenta" type="text" name='valorUnitario' />
-                </label>
-        
-                <label className="labelCampos"  htmlFor="estado">
-                    Estado
-                    <input name='estado' className="camposRegistroVenta" type="text" />
-                </label>                
-                
-        
-                <div className="contBotonGuardarVenta">
-                    
-                    <button className="botonCancelar" type="button" value="Cancelar" >Cancelar</button>
-                    <button onClick={enviarDatos} className="botonGuardar" type="button" value="Guardar">Crear</button>
-        
-                </div>          
-        
-            </form>
-  
-      </div>
+        </div>
+              
     );
 }
 
